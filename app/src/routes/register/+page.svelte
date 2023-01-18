740456ccbdd62d79a188159e1d133c647973565f
lebrancconvas <lebrancconvas@gmail.com>
2023-01-19 01:50:47 +0700

    ✨ feat: create registration form

--- app/src/routes/register/+page.svelte
+++ app/src/routes/register/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-
+	import { user } from '../../store/store.js';
 </script>
 
 <svelte:head>
@@ -12,6 +12,17 @@
 			<h1>Register</h1>
 		</div>
 	</header>
+	<section id="register-section">
+		<form>
+			<label>Username</label>
+			<input type="text" bind:value={$user.username} />
+			<label>Password</label>
+			<input type="password" bind:value={$user.password} /> 
+			<div>
+				<button>Register</button> 
+			</div>
+		</form>
+	</section>
 </main> 
 
 <style>
